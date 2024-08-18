b941a7ffa7d19ac3dacb67fe5b638a9d08f47234
Vitaliy new pc <vvoronik@yandex.ru>
2024-08-18 16:03:51 +0300

    feat set contact response

--- pages/contact.php
+++ pages/contact.php
@@ -24,12 +24,31 @@
 
 <?php
 if ($_POST) {
-    new \service\Telegram($_POST);
+    $token  = CONSTANTS['TELEGRAM_TOKEN'];
+    $chatId = CONSTANTS['TELEGRAM_CHATID'];
 
-    $content = "<h1>ff</h1>";
-    http_response_code(200);
-    header("Content-Type: application/json");
-    $data = json_encode(['success' => $content]);
-    exit($data);
+
+    $name   = \core\Clean::str($_POST['name']);
+    $phone  = \core\Clean::str($_POST['phone']);
+    $square = \core\Clean::int($_POST['square']);
+    $city   = \core\Clean::str($_POST['city']);
+    $option = \core\Clean::str($_POST['option']);
+    $text   =
+        "Наталиша, возьмешь еще заказ?" .
+        "Имя - $name" . PHP_EOL .
+        "тел - $phone" . PHP_EOL .
+        "площадь - $square" . PHP_EOL .
+        "город - $city" . PHP_EOL .
+        "тип - $option" . PHP_EOL;
+
+    $url = "https://api.telegram.org/bot$token/sendMessage?&chat_id=$chatId&text=$text";
+
+    header("Location:" . $url);
+//    new \service\Telegram($_POST);
+//    $content = "<h1>ff</h1>";
+//    http_response_code(200);
+//    header("Content-Type: application/json");
+//    $data = json_encode(['success' => $content]);
+//    exit($data);
 }
 ?>
