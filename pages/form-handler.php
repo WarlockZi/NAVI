1e16ff0de3a72aecb2826670825d8e818c7eccea
Vitaliy new pc <vvoronik@yandex.ru>
2024-08-18 14:28:32 +0300

    feat send message

--- pages/form-handler.php
+++ pages/form-handler.php
@@ -1,3 +1,6 @@
 <?php
-var_dump($_POST);
-exit('get - '. implode('\n',$_GET));
+$token = CONSTANTS['TELEGRAM_TOKEN'];
+$chatId = CONSTANTS['TELEGRAM_CHATID'];
+$url = "https://api.telegram.org/bot$token/sendMessage?&chat_id=$chatId&text=Ну привет, Виталий";
+
+header("Location:".$url);
