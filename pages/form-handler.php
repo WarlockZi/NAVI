bb9a2abccf17286db5b1d7d581d041784f3590eb
Vitaliy new pc <vvoronik@yandex.ru>
2024-08-18 14:42:54 +0300

    feat send message url

--- pages/form-handler.php
+++ pages/form-handler.php
@@ -1,6 +1,20 @@
 <?php
-$token = CONSTANTS['TELEGRAM_TOKEN'];
+$token  = CONSTANTS['TELEGRAM_TOKEN'];
 $chatId = CONSTANTS['TELEGRAM_CHATID'];
-$url = "https://api.telegram.org/bot$token/sendMessage?&chat_id=$chatId&text=Ну привет, Виталий";
 
-header("Location:".$url);
+
+$name   = \core\Clean::str($_POST['name']);
+$phone  = \core\Clean::str($_POST['phone']);
+$square = \core\Clean::int($_POST['square']);
+$city   = \core\Clean::str($_POST['city']);
+$option = \core\Clean::str($_POST['option']);
+$text   =
+    "Имя - $name" . PHP_EOL .
+    "тел - $phone" . PHP_EOL .
+    "площадь - $square" . PHP_EOL .
+    "город - $city" . PHP_EOL .
+    "тип - $option" . PHP_EOL;
+
+$url = "https://api.telegram.org/bot$token/sendMessage?&chat_id=$chatId&text=$text";
+
+header("Location:" . $url);
--- service/Telegram.php
+++ service/Telegram.php
@@ -6,15 +6,9 @@ use Longman\TelegramBot\Request;
 
 class Telegram
 {
-    private \Longman\TelegramBot\Telegram $service;
     public function __construct(array $post)
     {
-        $bot = $this->service = new \Longman\TelegramBot\Telegram(CONSTANTS["TELEGRAM_TOKEN"]);
 
-        Request::setCustomBotApiUri(
-            $api_base_uri          = 'https://your-bot-api-server', // Default: https://api.telegram.org
-            $api_base_download_uri = '/path/to/files/{API_KEY}'     // Default: /file/bot{API_KEY}
-        );
         $name   = \core\Clean::str($post['name']);
         $phone  = \core\Clean::str($post['phone']);
         $square = \core\Clean::int($post['square']);
